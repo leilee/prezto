f652ddec6879856151661946a2d28cbcf54a4a1f
edison <ll.buptee@gmail.com>
2017-09-02 19:58:47 +0800

    Fixed VS Code debugging issue.

--- modules/nvm/init.zsh
+++ modules/nvm/init.zsh
@@ -10,23 +10,37 @@ if [[ "$OSTYPE" != (darwin|linux)* ]]; then
   return 1
 fi
 
-lazynvm() {
-  unset -f nvm node npm
-  export NVM_DIR=~/.nvm
-  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+NVM_DIR="$HOME/.nvm"
+
+# Skip adding binaries if there is no node version installed yet
+if [ -d $NVM_DIR/versions/node ]; then
+  NODE_GLOBALS=(`find $NVM_DIR/versions/node -maxdepth 3 \( -type l -o -type f \) -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
+fi
+NODE_GLOBALS+=("nvm")
+
+load_nvm() {
+  # Unset placeholder functions
+  for cmd in "${NODE_GLOBALS[@]}"; do unset -f ${cmd} &>/dev/null; done
+
+  # Load NVM
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+  # (Optional) Set the version of node to use from ~/.nvmrc if available
+  nvm use 2> /dev/null 1>&2 || true
+
+  # Do not reload nvm again
+  export NVM_LOADED=1
 }
 
-nvm() {
-  lazynvm 
-  nvm $@
-}
- 
-node() {
-  lazynvm
-  node $@
-}
- 
-npm() {
-  lazynvm
-  npm $@
-}
+for cmd in "${NODE_GLOBALS[@]}"; do
+  # Skip defining the function if the binary is already in the PATH
+  if ! which ${cmd} &>/dev/null; then
+    eval "${cmd}() { unset -f ${cmd} &>/dev/null; [ -z \${NVM_LOADED+x} ] && load_nvm; ${cmd} \$@; }"
+  fi
+done
+
+# Only do lazy loading when NOT inside vscode
+# See also: https://github.com/Microsoft/vscode/issues/18556#issuecomment-299636934
+if [ -n "$VSCODE_PID" ]; then
+  load_nvm
+fi
