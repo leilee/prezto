5f2c152b315bc0a6bbf1d760c8da3c47a0f2ba1a
edison <ll.buptee@gmail.com>
2018-09-05 12:06:54 +0800

    Added git-extras plusin.

---
+++ modules/git-extras/README.md
@@ -0,0 +1,5 @@
+git-extras
+===
+
+[git-extras](https://github.com/tj/git-extras)
+
---
+++ modules/git-extras/init.zsh
@@ -0,0 +1,17 @@
+#
+# Configures git-extras dependency management.
+#
+# Authors: Edison <ll.buptee@gmail.com>
+#
+
+# Load git-extras completion
+git_extras_zsh_location="/usr/local/share/git-extras/"
+if [[ -s "${git_extras_zsh_location}/git-extras-completion.zsh" ]]; then 
+  source "${git_extras_zsh_location}/init.zsh" 2> /dev/null
+fi
+
+# workspace
+alias gw='git bulk -w'
+alias gwa='git bulk --addworkspace'
+alias gwd='git bulk --removeworkspace'
+alias gwl='git bulk --listall'
